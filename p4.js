0cf21331a61615ed1045d68033af2a2a9aa59350
Kate deBethune <kdebethune@gmail.com>
2015-12-09 22:08:00 -0500

    requirement 7, mobile gestures continued

--- p4.js
+++ p4.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 	$('main').bind('swipeleft', function(event){
-		$('#suzswipe').removeClass().addClass('one');
+		$('#suzswipe').removeClass().html('You just swiped left!').addClass('one');
 	}); //end swipe left
 
 	$('main').bind('swiperight', function(event){
-		$('#suzswipe').removeClass().addClass('two');
+		$('#suzswipe').removeClass().html('You just swiped right!').addClass('two');
 	}); //end swipe right
 
 	$('main').bind('taphold', function(event){
-		$('#suzswipe').removeClass().addClass('lots');
+		$('#suzswipe').removeClass().html('You\'re in the middle of a taphold now!').addClass('three');
 	}); //end taphold
 
 	$(window).on('orientationchange', function(){
